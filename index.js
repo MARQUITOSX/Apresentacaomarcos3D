af919392ef63a0ad1d0d9d9e9a9f1c90f17f0488
MARQUITOSX <116809028+MARQUITOSX@users.noreply.github.com>
2024-08-29 01:26:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const { log } = require("console");
+
+const primeiroNome = "Marcos";
+const sobrenome = "Vinicius";
+const idade = 17;
+const altura = 1.74;
+const cidadeNatal = "Sao Paulo";
+const massa = 60;
+const habilidade = "habilidades:";
+
+console.log("Meu primeiro Nome é:")
+console.log(primeiroNome);
+console.log("Já o meu sobrenome é:")
+console.log(sobrenome);
+console,log("Como nasci em 2007 no mês de abril tenho:")
+console.log(idade);
+console.log("Minha ultima medição de altura foi de:")
+console.log(altura);
+console.log("minha cidade natal é:")
+console.log(cidadeNatal);
+console.log("meu peso eu arredondei para baixo pois faz tempo que nao me peso, então e no minimo:")
+console.log(massa);
+console.log(habilidade);
+console.log("em dar grauzinhos de bike e volei")
